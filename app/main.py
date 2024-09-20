1c4b69836ed59ca01f8012e65d6efbacd89b6461
ruloalv <ruloalv@gmail.com>
2024-09-20 15:28:44 -0300

    adding comments to main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+# from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
@@ -7,7 +7,7 @@ from starlette.responses import HTMLResponse
 app = FastAPI()
 
 # Servir archivos estáticos como CSS y JS
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+# app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Configuración para utilizar plantillas Jinja2
 templates = Jinja2Templates(directory="app/templates")
