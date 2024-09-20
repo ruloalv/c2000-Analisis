0392b55c5e0ed16aba58cc2bfacb748e6340a487
ruloalv <ruloalv@gmail.com>
2024-09-20 15:37:59 -0300

    Simplify first run

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 # from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
-from starlette.responses import HTMLResponse
+# from fastapi.templating import Jinja2Templates
+# from starlette.requests import Request
+# from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -10,10 +10,14 @@ app = FastAPI()
 # app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Configuración para utilizar plantillas Jinja2
-templates = Jinja2Templates(directory="app/templates")
+# templates = Jinja2Templates(directory="app/templates")
 
 
-# Ruta para la página principal
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request})
+# Ruta para la página principal con Jinja2Templates
+# @app.get("/", response_class=HTMLResponse)
+# async def home(request: Request):
+#     return templates.TemplateResponse("base.html", {"request": request})
+
+@app.get("/")
+async def root():
+    return ("hello c2000")
