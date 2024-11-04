88666ee1acf500520bee92014a87d830b182220e
ruloalv <ruloalv@gmail.com>
2024-11-04 01:27:52 +0000

    creacion config.py

---
+++ app/config/config.py
@@ -0,0 +1,9 @@
+# Configuración de conexión
+DB_HOST = "mysql-u-ng.railway.internal"
+DB_PORT = "3306"
+DB_USER = "root"
+DB_PASSWORD = "dbLnhTjzsETZAlKEBfKoVFeCJfKsiDUw"
+DB_NAME = "MySQL-U-NG"
+
+# Cadena de conexión
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
--- app/routers/proveedores.py
+++ app/routers/proveedores.py
@@ -12,10 +12,12 @@ class Proveedor(BaseModel):
     id: int
     name: str
     location: str
+    cuit: int
+    active: bool
 
-proveedores_list = [Proveedor(id=1, name="Trafigura", location="Bahia Blanca"),
-                  Proveedor(id=2, name="Piro y Ruiz S.R.L.", location="Bahia Blanca"),
-                  Proveedor(id=3, name="Roza Hnos. S.A.", location="Bahia Blanca")]
+proveedores_list = [Proveedor(id=1, name="Trafigura", location="Bahia Blanca", cuit="30311221581", active="true"),
+                  Proveedor(id=2, name="Piro y Ruiz S.R.L.", location="Bahia Blanca", cuit="30311221581", active="true"),
+                  Proveedor(id=3, name="Roza Hnos. S.A.", location="Bahia Blanca", cuit="30311221581", active="true")]
 
 @router.get("/", response_class=HTMLResponse)
 async def obtener_proveedores(request: Request):
